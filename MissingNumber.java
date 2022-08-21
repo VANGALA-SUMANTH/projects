cfd6e32e200ea73966e3313d167aab5cbd87e940
sumanth <sumanthvangala7013@gmail.com>
2022-08-21 13:31:41 +0530

    mising numbers code

---
+++ MissingNumber.java
@@ -0,0 +1,25 @@
+package com.codes;
+
+public class MissingNumber {
+  
+	public static void main(String[] args) {
+		int [] a= {6,2,9,10,8,1,5,3,4};
+      
+/*	for(int i=1;i<=10;i++) {
+		 boolean bool=false;
+		for(int j=0;j<a.length;j++) {
+			if(a[j]==i) {
+				bool=true;
+		    break;
+			}
+		}
+		if(bool==false)
+			System.out.println(i+" digit is misiing");
+	}
+*/
+		
+	
+	
+	
+	}
+}
